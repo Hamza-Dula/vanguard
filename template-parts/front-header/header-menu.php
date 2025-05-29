a42a0dcc34119052d14e10a11c54155c01976d48
Andrei Sestac <andrei.sestac@extendstudio.com>
2025-05-29 12:19:15 +0300

    0058520: Edit button for Header menu is missing
    https://mantis.iconvert.pro/view.php?id=58520

--- template-parts/front-header/header-menu.php
+++ template-parts/front-header/header-menu.php
@@ -1,67 +1,67 @@
 <?php $component = \ColibriWP\Theme\View::getData( 'component' ); ?>
-<div class="wp-block wp-block-kubio-dropdown-menu  position-relative wp-block-kubio-dropdown-menu__outer monivo-header__k__KxNZuItwrB_-outer monivo-local-671-outer kubio-dropdown-menu h-ignore-global-body-typography <?php echo $component->printHeaderMenuClasses(); ?>" data-kubio="kubio/dropdown-menu" data-kubio-component="dropdown-menu" data-kubio-settings="[]">
-	<div class="wp-block wp-block-kubio-menu-items  position-relative wp-block-kubio-menu-items__outer monivo-header__k__YTkcdxOu1zs-outer monivo-local-672-outer kubio-menu" data-kubio="kubio/menu-items">
+<div class="wp-block wp-block-kubio-dropdown-menu  position-relative wp-block-kubio-dropdown-menu__outer monivo-front-header__k__qr6B7XVdwN-outer monivo-local-150-outer kubio-dropdown-menu h-ignore-global-body-typography <?php echo $component->printHeaderMenuClasses(); ?>" data-kubio="kubio/dropdown-menu" data-kubio-component="dropdown-menu" data-kubio-settings="[]">
+	<div class="wp-block wp-block-kubio-menu-items  position-relative wp-block-kubio-menu-items__outer monivo-front-header__k__tTI7QY4tW9-outer monivo-local-151-outer kubio-menu" data-kubio="kubio/menu-items">
 		<?php $component->printHeaderMenu(); ?>
 	</div>
-	<div class="wp-block wp-block-kubio-menu-offscreen  position-relative wp-block-kubio-menu-offscreen__container monivo-header__k__u1icTKAciGU-container monivo-local-673-container" data-kubio="kubio/menu-offscreen">
-		<div class="position-relative wp-block-kubio-menu-offscreen__align monivo-header__k__u1icTKAciGU-align monivo-local-673-align">
-			<button class="position-relative wp-block-kubio-menu-offscreen__iconWrapper monivo-header__k__u1icTKAciGU-iconWrapper monivo-local-673-iconWrapper kubio-offscreen-icon-wrapper" data-target="#kubio-offscreen-682583e878ffd" data-target-id="kubio-offscreen-682583e878ffd" data-direction="right" data-width="300px" data-offcanvas-overlay-id="kubio-offscreen-682583e878ffd-overlay" aria-label="Mobile Menu" data-kubio-component="offcanvas" data-kubio-settings="[]">
-				<span class="h-svg-icon wp-block-kubio-menu-offscreen__icon monivo-header__k__u1icTKAciGU-icon monivo-local-673-icon" name="font-awesome/navicon">
+	<div class="wp-block wp-block-kubio-menu-offscreen  position-relative wp-block-kubio-menu-offscreen__container monivo-front-header__k__NrnxJF4jP4-container monivo-local-152-container" data-kubio="kubio/menu-offscreen">
+		<div class="position-relative wp-block-kubio-menu-offscreen__align monivo-front-header__k__NrnxJF4jP4-align monivo-local-152-align">
+			<button class="position-relative wp-block-kubio-menu-offscreen__iconWrapper monivo-front-header__k__NrnxJF4jP4-iconWrapper monivo-local-152-iconWrapper kubio-offscreen-icon-wrapper" data-target="#kubio-offscreen-6825967251a24" data-target-id="kubio-offscreen-6825967251a24" data-direction="right" data-width="300px" data-offcanvas-overlay-id="kubio-offscreen-6825967251a24-overlay" aria-label="Mobile Menu" data-kubio-component="offcanvas" data-kubio-settings="[]">
+				<span class="h-svg-icon wp-block-kubio-menu-offscreen__icon monivo-front-header__k__NrnxJF4jP4-icon monivo-local-152-icon" name="font-awesome/navicon">
 					<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="navicon" viewBox="0 0 1536 1896.0833">
 						<path d="M1536 1344v128q0 26-19 45t-45 19H64q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H64q-26 0-45-19T0 960V832q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H64q-26 0-45-19T0 448V320q0-26 19-45t45-19h1408q26 0 45 19t19 45z"/></svg>
 					</span>
 				</button>
 			</div>
-			<div class="position-relative wp-block-kubio-menu-offscreen__offscreenOverlay monivo-header__k__u1icTKAciGU-offscreenOverlay monivo-local-673-offscreenOverlay kubio-offscreen-overlay offscreen-overlay" id="kubio-offscreen-682583e878ffd-overlay"></div>
-			<div class="position-relative wp-block-kubio-menu-offscreen__offscreen monivo-header__k__u1icTKAciGU-offscreen monivo-local-673-offscreen kubio-offscreen offscreen hide" id="kubio-offscreen-682583e878ffd">
-				<div class="position-relative wp-block-kubio-menu-offscreen__inner monivo-header__k__u1icTKAciGU-inner monivo-local-673-inner">
-					<div class="wp-block wp-block-kubio-menu-offscreen-content  position-relative wp-block-kubio-menu-offscreen-content__container monivo-header__k__GEAKJMBnLBw-container monivo-local-674-container" data-kubio="kubio/menu-offscreen-content">
-						<div class="wp-block wp-block-kubio-row  position-relative wp-block-kubio-row__container monivo-header__k__STfmGV6iN-B-container monivo-local-675-container gutters-row-lg-2 gutters-row-v-lg-2 gutters-row-md-2 gutters-row-v-md-2 gutters-row-0 gutters-row-v-2" data-kubio="kubio/row">
+			<div class="position-relative wp-block-kubio-menu-offscreen__offscreenOverlay monivo-front-header__k__NrnxJF4jP4-offscreenOverlay monivo-local-152-offscreenOverlay kubio-offscreen-overlay offscreen-overlay" id="kubio-offscreen-6825967251a24-overlay"></div>
+			<div class="position-relative wp-block-kubio-menu-offscreen__offscreen monivo-front-header__k__NrnxJF4jP4-offscreen monivo-local-152-offscreen kubio-offscreen offscreen hide" id="kubio-offscreen-6825967251a24">
+				<div class="position-relative wp-block-kubio-menu-offscreen__inner monivo-front-header__k__NrnxJF4jP4-inner monivo-local-152-inner">
+					<div class="wp-block wp-block-kubio-menu-offscreen-content  position-relative wp-block-kubio-menu-offscreen-content__container monivo-front-header__k__9z2kuPxsw--container monivo-local-153-container" data-kubio="kubio/menu-offscreen-content">
+						<div class="wp-block wp-block-kubio-row  position-relative wp-block-kubio-row__container monivo-front-header__k__lFxNAW4npJ-container monivo-local-154-container gutters-row-lg-2 gutters-row-v-lg-2 gutters-row-md-2 gutters-row-v-md-2 gutters-row-0 gutters-row-v-2" data-kubio="kubio/row">
 							<div class="background-wrapper">
 								<div class="background-layer background-layer-media-container-lg"></div>
 								<div class="background-layer background-layer-media-container-md"></div>
 								<div class="background-layer background-layer-media-container"></div>
 							</div>
-							<div class="position-relative wp-block-kubio-row__inner monivo-header__k__STfmGV6iN-B-inner monivo-local-675-inner h-row align-items-lg-stretch align-items-md-stretch align-items-stretch justify-content-lg-center justify-content-md-center justify-content-center gutters-col-lg-2 gutters-col-v-lg-2 gutters-col-md-2 gutters-col-v-md-2 gutters-col-0 gutters-col-v-2">
-								<div class="wp-block wp-block-kubio-column  position-relative wp-block-kubio-column__container monivo-header__k__vipL2rC09xz-container monivo-local-676-container d-flex h-col-lg h-col-md h-col-auto" data-kubio="kubio/column">
-									<div class="position-relative wp-block-kubio-column__inner monivo-header__k__vipL2rC09xz-inner monivo-local-676-inner d-flex h-flex-basis h-px-lg-2 v-inner-lg-2 h-px-md-2 v-inner-md-2 h-px-2 v-inner-2">
+							<div class="position-relative wp-block-kubio-row__inner monivo-front-header__k__lFxNAW4npJ-inner monivo-local-154-inner h-row align-items-lg-stretch align-items-md-stretch align-items-stretch justify-content-lg-center justify-content-md-center justify-content-center gutters-col-lg-2 gutters-col-v-lg-2 gutters-col-md-2 gutters-col-v-md-2 gutters-col-0 gutters-col-v-2">
+								<div class="wp-block wp-block-kubio-column  position-relative wp-block-kubio-column__container monivo-front-header__k__LkTYLRUjTwT-container monivo-local-155-container d-flex h-col-lg h-col-md h-col-auto" data-kubio="kubio/column">
+									<div class="position-relative wp-block-kubio-column__inner monivo-front-header__k__LkTYLRUjTwT-inner monivo-local-155-inner d-flex h-flex-basis h-px-lg-2 v-inner-lg-2 h-px-md-2 v-inner-md-2 h-px-2 v-inner-2">
 										<div class="background-wrapper">
 											<div class="background-layer background-layer-media-container-lg"></div>
 											<div class="background-layer background-layer-media-container-md"></div>
 											<div class="background-layer background-layer-media-container"></div>
 										</div>
-										<div class="position-relative wp-block-kubio-column__align monivo-header__k__vipL2rC09xz-align monivo-local-676-align h-y-container h-column__content h-column__v-align flex-basis-100 align-self-lg-start align-self-md-start align-self-start">
+										<div class="position-relative wp-block-kubio-column__align monivo-front-header__k__LkTYLRUjTwT-align monivo-local-155-align h-y-container h-column__content h-column__v-align flex-basis-100 align-self-lg-start align-self-md-start align-self-start">
 											<?php monivo_theme()->get('logo')->render(array (
-  'wrapper_class' => 'wp-block wp-block-kubio-logo position-relative wp-block-kubio-logo__container  kubio-logo-direction-row monivo-header__k__-BPNpcJAqpN-container monivo-local--container',
-  'logo_image_class' => 'position-relative wp-block-kubio-logo__image  kubio-logo-image  monivo-header__k__-BPNpcJAqpN-image monivo-local--image',
-  'alt_logo_image_class' => 'position-relative wp-block-kubio-logo__alternateImage kubio-logo-image kubio-alternate-logo-image   monivo-header__k__-BPNpcJAqpN-alternateImage monivo-local--alternateImage',
-  'logo_text_class' => 'position-relative wp-block-kubio-logo__text  monivo-header__k__-BPNpcJAqpN-text monivo-local--text',
+  'wrapper_class' => 'wp-block wp-block-kubio-logo position-relative wp-block-kubio-logo__container  kubio-logo-direction-row monivo-front-header__k__6naQ2A99kL3-container monivo-local--container',
+  'logo_image_class' => 'position-relative wp-block-kubio-logo__image  kubio-logo-image  monivo-front-header__k__6naQ2A99kL3-image monivo-local--image',
+  'alt_logo_image_class' => 'position-relative wp-block-kubio-logo__alternateImage kubio-logo-image kubio-alternate-logo-image   monivo-front-header__k__6naQ2A99kL3-alternateImage monivo-local--alternateImage',
+  'logo_text_class' => 'position-relative wp-block-kubio-logo__text  monivo-front-header__k__6naQ2A99kL3-text monivo-local--text',
 )); ?>
 										</div>
 									</div>
 								</div>
 							</div>
 						</div>
-						<div class="wp-block wp-block-kubio-accordion-menu  position-relative wp-block-kubio-accordion-menu__outer monivo-header__k__e0NESk9PZSk-outer monivo-local-678-outer kubio-accordion-menu h-ignore-global-body-typography" data-kubio="kubio/accordion-menu" data-kubio-component="accordion-menu" data-kubio-settings="[]">
-							<div class="wp-block wp-block-kubio-menu-items  position-relative wp-block-kubio-menu-items__outer monivo-header__k__mPH07zJoL2C-outer monivo-local-679-outer kubio-menu" data-kubio="kubio/menu-items">
+						<div class="wp-block wp-block-kubio-accordion-menu  position-relative wp-block-kubio-accordion-menu__outer monivo-front-header__k__0OmfqTKOlNl-outer monivo-local-157-outer kubio-accordion-menu h-ignore-global-body-typography" data-kubio="kubio/accordion-menu" data-kubio-component="accordion-menu" data-kubio-settings="[]">
+							<div class="wp-block wp-block-kubio-menu-items  position-relative wp-block-kubio-menu-items__outer monivo-front-header__k__ZA2G_eiTlX9-outer monivo-local-158-outer kubio-menu" data-kubio="kubio/menu-items">
 								<?php $component->printHeaderMenu(); ?>
 							</div>
 						</div>
-						<div class="wp-block wp-block-kubio-row  position-relative wp-block-kubio-row__container monivo-header__k__8XJ93d4t7wn-container monivo-local-680-container gutters-row-lg-2 gutters-row-v-lg-2 gutters-row-md-2 gutters-row-v-md-2 gutters-row-0 gutters-row-v-2" data-kubio="kubio/row">
+						<div class="wp-block wp-block-kubio-row  position-relative wp-block-kubio-row__container monivo-front-header__k__-OeI-LFENdB-container monivo-local-159-container gutters-row-lg-2 gutters-row-v-lg-2 gutters-row-md-2 gutters-row-v-md-2 gutters-row-0 gutters-row-v-2" data-kubio="kubio/row">
 							<div class="background-wrapper">
 								<div class="background-layer background-layer-media-container-lg"></div>
 								<div class="background-layer background-layer-media-container-md"></div>
 								<div class="background-layer background-layer-media-container"></div>
 							</div>
-							<div class="position-relative wp-block-kubio-row__inner monivo-header__k__8XJ93d4t7wn-inner monivo-local-680-inner h-row align-items-lg-stretch align-items-md-stretch align-items-stretch justify-content-lg-center justify-content-md-center justify-content-center gutters-col-lg-2 gutters-col-v-lg-2 gutters-col-md-2 gutters-col-v-md-2 gutters-col-0 gutters-col-v-2">
-								<div class="wp-block wp-block-kubio-column  position-relative wp-block-kubio-column__container monivo-header__k__yxs9hKufCOT-container monivo-local-681-container d-flex h-col-lg-auto h-col-md-auto h-col-auto" data-kubio="kubio/column">
-									<div class="position-relative wp-block-kubio-column__inner monivo-header__k__yxs9hKufCOT-inner monivo-local-681-inner d-flex h-flex-basis h-px-lg-2 v-inner-lg-2 h-px-md-2 v-inner-md-2 h-px-2 v-inner-2">
+							<div class="position-relative wp-block-kubio-row__inner monivo-front-header__k__-OeI-LFENdB-inner monivo-local-159-inner h-row align-items-lg-stretch align-items-md-stretch align-items-stretch justify-content-lg-center justify-content-md-center justify-content-center gutters-col-lg-2 gutters-col-v-lg-2 gutters-col-md-2 gutters-col-v-md-2 gutters-col-0 gutters-col-v-2">
+								<div class="wp-block wp-block-kubio-column  position-relative wp-block-kubio-column__container monivo-front-header__k__oDry5raECEa-container monivo-local-160-container d-flex h-col-lg-auto h-col-md-auto h-col-auto" data-kubio="kubio/column">
+									<div class="position-relative wp-block-kubio-column__inner monivo-front-header__k__oDry5raECEa-inner monivo-local-160-inner d-flex h-flex-basis h-px-lg-2 v-inner-lg-2 h-px-md-2 v-inner-md-2 h-px-2 v-inner-2">
 										<div class="background-wrapper">
 											<div class="background-layer background-layer-media-container-lg"></div>
 											<div class="background-layer background-layer-media-container-md"></div>
 											<div class="background-layer background-layer-media-container"></div>
 										</div>
-										<div class="position-relative wp-block-kubio-column__align monivo-header__k__yxs9hKufCOT-align monivo-local-681-align h-y-container h-column__content h-column__v-align flex-basis-100 align-self-lg-start align-self-md-start align-self-start">
+										<div class="position-relative wp-block-kubio-column__align monivo-front-header__k__oDry5raECEa-align monivo-local-160-align h-y-container h-column__content h-column__v-align flex-basis-100 align-self-lg-start align-self-md-start align-self-start">
 											<?php monivo_theme_print_footer_copyright(true); ?>
 										</div>
 									</div>
